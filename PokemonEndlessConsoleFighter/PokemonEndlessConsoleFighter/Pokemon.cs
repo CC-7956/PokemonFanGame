9b94f4c0bade092cf337dc3fde75c04ed6ad06fb
CC-7956 <222263206+CC-7956@users.noreply.github.com>
2025-09-23 08:15:55 +0200

    Initial commit

---
+++ PokemonEndlessConsoleFighter/PokemonEndlessConsoleFighter/Pokemon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonEndlessConsoleFighter
+{
+    internal class Pokemon
+    {
+        public string _name { get; set; }
+        public int _index { get; set; }
+        public bool _shiny { get; set; }
+
+        public Pokemon(Pokedex pokemon)
+        {
+            _name = pokemon.ToString();
+            _index = (int)pokemon;
+            _shiny = (new Random().Next(1, 8193) == 1); // 1 in 8192 chance
+        }
+
+        public override string ToString()
+        {
+            if (_shiny)
+            {
+                return $"A wild shiny {_name} (#{_index}) appeared!";
+            }
+            else
+            {
+                return $"A wild {_name} (#{_index}) appeared!";
+            }
+        }
+    }
+}
---
+++ PokemonEndlessConsoleFighter/PokemonEndlessConsoleFighter/Runner.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+
+namespace PokemonEndlessConsoleFighter
+{
+    internal class Runner
+    {
+        static void Main(string[] args)
+        {
+            int shinys = 0;
+            Dictionary<Pokedex, int> encounter = new Dictionary<Pokedex, int>();
+            for (int i = 0; i < 100_000; i++)
+            {
+                Pokedex randomPokemon = GetRandomPokemon();
+                Pokemon newPokemon = new Pokemon(randomPokemon);
+                PrintPokemon(newPokemon);
+                if(newPokemon._shiny)
+                {
+                    shinys++;
+                    if (encounter.ContainsKey(randomPokemon))
+                    {
+                        encounter[randomPokemon]++;
+                    }
+                    else
+                    {
+                        encounter[randomPokemon] = 1;
+                    }
+                }
+                Console.WriteLine("Press Enter to encounter another Pokémon, or type 'exit' to quit.");
+                Task.Delay(50).Wait();
+                Console.Clear();
+            }
+            Console.WriteLine($"You have encountered {shinys} shiny Pokémon!");
+            foreach (var pokemon in encounter)
+            {
+                Console.WriteLine($"{pokemon.Key}: {pokemon.Value}");
+            }
+        }
+
+        private static Pokedex GetRandomPokemon()
+        {
+            Random rand = new Random();
+            int pokeNumber = rand.Next(1, 152); // 1-151 inclusive
+            return (Pokedex)pokeNumber;
+        }
+
+        private static void PrintPokemon(Pokemon pokemon)
+        {
+            if (pokemon._shiny)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            Console.WriteLine(pokemon.ToString());
+            Console.ResetColor();
+        }
+    }
+}
